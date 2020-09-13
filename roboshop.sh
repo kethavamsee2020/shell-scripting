f72d222a966e7a506bfeace8faf00b19a9df4bf1
admin <kethavamsee2020>
2020-09-13 17:09:01 -0400

    Robo shop project

--- roboshop.sh
+++ roboshop.sh
@@ -1,20 +1,20 @@
 #!/bin/bash
 
 case $1 in
-Frontend)
-  echo Installing frontend
+ Frontend)
+   echo Installing frontend
   yum install nginx -y
   # echo Completed installation of frontend
   ;;
-Catalogue)
-  echo installing catalogue
-  echo Completed installtion of Catalogue
+ Catalogue)
+   echo installing catalogue
+   echo Completed installtion of Catalogue
   ;;
-Cart)
-  echo installing Cart
-  echo Completed installtion of Cart
+ Cart)
+   echo installing Cart
+   echo Completed installtion of Cart
   ;;
-*)
-  echo "invalid inputs, flowing inputs are only accepted"
-  echo "usage:$0 frontend|catalogue|cart"
+ *)
+   echo "invalid inputs, flowing inputs are only accepted"
+   echo "usage:$0 frontend|catalogue|cart"
 esac
